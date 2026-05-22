96316c0a22c8e08d4b8738de6f53b386fd6080b4
Devil codes <h34351449@gmail.com>
2026-05-22 11:09:58 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ pip install beautifulsoup4 requests --break-system-packages
 echo 'python $HOME/scanner.py' > $PREFIX/bin/somesites
 chmod +x $PREFIX/bin/somesites
 echo "Installation complete! Type 'somesites' to run."
+#Then type chmod +x setup.sh
